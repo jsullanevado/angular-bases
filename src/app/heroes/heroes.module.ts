8182b40568fab8012a0dcd8b16043830b8578db0
Jose Sulla <jsullanevado@gmail.com>
2023-07-10 16:46:30 -0500

    }

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+
 import { HeroComponent } from './hero/hero.component';
 import { ListComponent } from './list/list.component';
 
